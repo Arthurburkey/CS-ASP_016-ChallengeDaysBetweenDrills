cdb08bee92eafe269f8850e516b49fde34d863ff
Arthurburkey <Arthurburkey@email.com>
2017-07-22 23:33:07 -0700

    C# Course slide-16 drill
    Using two calendar functions, the user can display how many days occured
    between selected dates.

---
+++ CS-ASP_016-ChallengeDaysBetweenDrills/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_016_ChallengeDaysBetweenDrills
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calendar2_SelectionChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = Calendar2.SelectedDate
+                .Subtract(Calendar1.SelectedDate)
+                .TotalDays.ToString();
+        }
+    }
+}
